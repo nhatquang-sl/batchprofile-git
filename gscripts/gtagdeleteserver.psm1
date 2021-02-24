e138d317ab4609630d4fccf0b10fd5de903906e8
Quang Nguyen <quang.nguyen@fmgsuite.com>
2021-02-24 07:44:25 +0700

    Update gtagdeleteserver

--- gscripts/gtagdeleteserver.psm1
+++ gscripts/gtagdeleteserver.psm1
@@ -1,9 +1,13 @@
 function gtagdeleteserver([string]$tagInput) {
     if (-not ([string]::IsNullOrEmpty($tagInput))) {
+        $cmd = "git tag -d $tagInput"
+        RunBatchCmd $cmd
+
         $cmd = "git push origin --delete $tagInput"
         RunBatchCmd $cmd
     } else {
         $branches = git tag
+        SearchItemOfArray -CmdArray $branches -CmdTemplate 'git tag -d [cmdSelected]'
         SearchItemOfArray -CmdArray $branches -CmdTemplate 'git push origin --delete [cmdSelected]'
     }
 }
